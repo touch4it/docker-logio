bf7f70032fe6c4145153472438fe2f1f8ae756ca
Touch4IT <hi@touch4it.com>
2017-07-27 20:27:13 +0200

    Use Rancher stack/service names instead of container image/id

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ var through = require('through2');
 var os = require("os");
 
 program
-  .version('0.1.0')
-  .option('-h, --host [host]', 'LogIo server host', 'localhost')
-  .option('-p, --port [port]', 'LogIo server port', 28777)
-  .option('-n, --name [name]', 'LogIo node name', os.hostname())
+  .version('0.4.0')
+  .option('-h, --host [host]', 'Log.io server host', 'localhost')
+  .option('-p, --port [port]', 'Log.io server port', 28777)
+  .option('-n, --name [name]', 'Log.io node name', os.hostname())
   .parse(process.argv);
-
-
 
 function connect() {
   var stream = net.createConnection(program.port, program.host);
@@ -32,16 +30,25 @@ var opts = {
   docker: null,
   events: ee
 };
-loghose(opts).pipe(through.obj(function(chunk, enc, cb){
-  this.push('+log|'+chunk.image+'|'+program.name+':'+chunk.id+'|info|'+chunk.line+'\r\n');
+
+loghose(opts).pipe(through.obj(function(chunk, enc, cb) {
+  var names = chunk.name.match(/^r-(.*)-([A-Za-z0-9]+)-[0-9]+-[0-9a-z]+$/);
+  if (Array.isArray(names) && names.length > 2) {
+    this.push('+log|'+names[1]+'|'+names[2]+'|info|'+chunk.line+'\r\n');
+  }
   cb();
 })).pipe(s);
 
 ee.on('start', function(meta, container) {
-  var id = meta.id.substring(0, 12);
-  s.write('+node|'+program.name+':'+id+'|'+meta.image+'\r\n');
+  var names = meta.name.match(/^r-(.*)-([A-Za-z0-9]+)-[0-9]+-[0-9a-z]+$/);
+  if (Array.isArray(names) && names.length > 2) {
+    s.write('+node|'+names[2]+'|'+names[1]+'\r\n');
+  }
 });
+
 ee.on('stop', function(meta, container) {
-  var id = meta.id.substring(0, 12);
-  s.write('-node|'+program.name+':'+id+'\r\n');
+  var names = meta.name.match(/^r-(.*)-([A-Za-z0-9]+)-[0-9]+-[0-9a-z]+$/);
+  if (Array.isArray(names) && names.length > 2) {
+    s.write('-node|'+names[2]+'\r\n');
+  }
 });
